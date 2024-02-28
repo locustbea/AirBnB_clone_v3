ccf4655d95f26f28655367f7bfdfbbbd0df7032c
root <you@example.com>
2024-02-28 13:17:17 -0800

    add them

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,97 +1,59 @@
 #!/usr/bin/python3
-"""
-route for handling State objects and operations
-"""
+"""handles all default RESTFul API actions for State objects"""
 from flask import jsonify, abort, request
-from api.v1.views import app_views, storage
+from api.v1.views import app_views
+from models import storage
 from models.state import State
+from api.v1.views.base_actions import REST_actions
 
 
-@app_views.route("/states", methods=["GET"], strict_slashes=False)
-def state_get_all():
-    """
-    retrieves all State objects
-    :return: json of all states
-    """
-    state_list = []
-    state_obj = storage.all("State")
-    for obj in state_obj.values():
-        state_list.append(obj.to_json())
-
-    return jsonify(state_list)
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """gets all State objects"""
+    return jsonify(REST_actions.get(State))
 
 
-@app_views.route("/states", methods=["POST"], strict_slashes=False)
-def state_create():
-    """
-    create state route
-    :return: newly created state obj
-    """
-    state_json = request.get_json(silent=True)
-    if state_json is None:
-        abort(400, 'Not a JSON')
-    if "name" not in state_json:
-        abort(400, 'Missing name')
-
-    new_state = State(**state_json)
-    new_state.save()
-    resp = jsonify(new_state.to_json())
-    resp.status_code = 201
-
-    return resp
+@app_views.route('/states', methods=['POST'])
+def post_state():
+    """creates a State"""
+    request_body = request.get_json()
+    if not request_body:
+        abort(400, "Not a JSON")
+    if not request_body.get('name'):
+        abort(400, "Missing name")
+    new_state = State(name=request_body.get('name'))
+    post_response = REST_actions.post(new_state)
+    return post_response.get('object dict'), post_response.get('status code')
 
 
-@app_views.route("/states/<state_id>",  methods=["GET"], strict_slashes=False)
-def state_by_id(state_id):
-    """
-    gets a specific State object by ID
-    :param state_id: state object id
-    :return: state obj with the specified id or error
-    """
-
-    fetched_obj = storage.get("State", str(state_id))
-
-    if fetched_obj is None:
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """gets a State object by its id"""
+    state = REST_actions.get_by_id(State, state_id)
+    if state.get('status code') == 404:
         abort(404)
+    return jsonify(state.get('object dict'))
 
-    return jsonify(fetched_obj.to_json())
 
-
-@app_views.route("/states/<state_id>",  methods=["PUT"], strict_slashes=False)
-def state_put(state_id):
-    """
-    updates specific State object by ID
-    :param state_id: state object ID
-    :return: state object and 200 on success, or 400 or 404 on failure
-    """
-    state_json = request.get_json(silent=True)
-    if state_json is None:
-        abort(400, 'Not a JSON')
-    fetched_obj = storage.get("State", str(state_id))
-    if fetched_obj is None:
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """deletes a State object by its id"""
+    delete_response = REST_actions.delete(State, state_id)
+    if delete_response.get('status code') == 404:
         abort(404)
-    for key, val in state_json.items():
-        if key not in ["id", "created_at", "updated_at"]:
-            setattr(fetched_obj, key, val)
-    fetched_obj.save()
-    return jsonify(fetched_obj.to_json())
-
-
-@app_views.route("/states/<state_id>", methods=["DELETE"],
-                 strict_slashes=False)
-def state_delete_by_id(state_id):
-    """
-    deletes State by id
-    :param state_id: state object id
-    :return: empty dict with 200 or 404 if not found
-    """
-
-    fetched_obj = storage.get("State", str(state_id))
-
-    if fetched_obj is None:
-        abort(404)
-
-    storage.delete(fetched_obj)
-    storage.save()
-
     return jsonify({})
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def put_state(state_id):
+    """ updates a State object by its id """
+    request_body = request.get_json()
+    if not request_body:
+        return jsonify({'error': 'Not a JSON'}), 400
+
+    args_to_ignore = ['id', 'created_at', 'updated_at']
+    put_response = REST_actions.put(
+        State, state_id, args_to_ignore, request_body)
+    if put_response.get('status code') == 404:
+        abort(404)
+    return put_response.get('object dict'), put_response.get('status code')
